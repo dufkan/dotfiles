8c85dab4cd0912814ec21d91f36f2aa0f5db7262
Antonín Dufka <xdufka1@fi.muni.cz>
2019-08-27 12:37:41 +0200

    Add rofi menus

---
+++ omega/rofi/scripts/appsmenu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rofi -no-lazy-grab -show drun -theme themes/appsmenu.rasi
+
---
+++ omega/rofi/scripts/scrotmenu.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rofi_command="rofi -theme themes/scrotmenu.rasi"
+
+screen=""
+area=""
+window=""
+
+options="$area\n$screen\n$window"
+
+chosen="$(echo -e "$options" | $rofi_command -dmenu -selected-row 1)"
+case $chosen in
+    $screen)
+        sleep 3; scrot --quality 100 ~/Screenshots/screen-`date +%s`.jpg && notify-send --urgency=normal "Screenshot" "New screenshot taken"
+        ;;
+    $area)
+        scrot -s --quality 100 ~/Screenshots/screen-`date +%s`.jpg
+        ;;
+    $window)
+        sleep 3; scrot -u --quality 100 ~/Screenshots/screen-`date +%s`.jpg && notify-send --urgency=normal "Screenshot" "New screenshot taken"
+
+        ;;
+esac
+
