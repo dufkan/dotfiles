e213714f88007a90e007f8a4309c996eef9c12ce
Antonín Dufka <xdufka1@fi.muni.cz>
2020-02-16 18:56:23 +0100

    Ω Improve dunst

--- omega/polybar/dunst.sh
+++ omega/polybar/dunst.sh
@@ -4,7 +4,7 @@ if [ "$1" = "click" ]; then
     if [ -f /tmp/dunst_pause ]; then
         killall -SIGUSR2 dunst
         rm -f /tmp/dunst_pause
-        notify-send --urgency=normal "Dunst" "Dunst Resumed"
+        notify-send --urgency=normal "Notifications" "Notifications Resumed"
     else
         killall -SIGUSR1 dunst
         touch /tmp/dunst_pause
